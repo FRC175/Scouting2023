54d7bb9bc6843990ac1a7c9979df7e421e58eb73
Anthony5243 <tonygalvez23@gmail.com>
2023-11-08 19:10:24 -0500

    The entire project

---
+++ app/src/main/java/com/example/hellachanges/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hellachanges;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String s = "";
+
+        Button submit = (Button) findViewById(R.id.button);
+
+
+    }
+}
+
